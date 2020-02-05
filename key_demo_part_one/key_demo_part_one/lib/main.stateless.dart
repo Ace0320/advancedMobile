f1d17c1e9464f99372dce89f5b2380c02d19fdf0
Kenny Grannas <s526521@nwmissouri.edu>
2020-02-05 08:30:42 -0600

    inClass

---
+++ key_demo_part_one/key_demo_part_one/lib/main.stateless.dart
@@ -0,0 +1,53 @@
+/*import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MaterialApp(home: PositionedTiles()));
+
+class PositionedTiles extends StatefulWidget {
+ @override
+ State<StatefulWidget> createState() => PositionedTilesState();
+}
+
+class PositionedTilesState extends State<PositionedTiles> {
+ List<Widget> tiles = [
+   StatelessColorfulTile(),
+   StatelessColorfulTile(),
+ ];
+
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     body: Column(children: [Spacer(flex:1), Row(children: tiles), Spacer(flex:10)]),
+     floatingActionButton: FloatingActionButton(
+         child: Icon(Icons.sentiment_very_satisfied), onPressed: swapTiles),
+   );
+ }
+
+ swapTiles() {
+   setState(() {
+     tiles.insert(1, tiles.removeAt(0));
+   });
+ }
+}
+
+class StatelessColorfulTile extends StatelessWidget {
+ Color myColor = UniqueColorGenerator.getColor();
+ @override
+ Widget build(BuildContext context) {
+   return Container(
+       color: myColor, child: Padding(padding: EdgeInsets.all(70.0)));
+ }
+}
+
+
+class UniqueColorGenerator{
+  static Random randy = Random();
+  static List<Color> colors = [Colors.red, Colors.orange, Colors.yellow, Colors.green, Colors.blue, Colors.purple];
+  static Color getColor(){
+    return Color.fromARGB(255, randy.nextInt(256), randy.nextInt(256), randy.nextInt(256));
+    // return Color.fromARGB(255, 255, randy.nextInt(256), 0);
+  }
+}
+
+*/
